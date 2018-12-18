dbe238f07f67ad39a84427f3d28381389b7487af
Bastien Vacherand <coco2053@hotmail.com>
2018-12-18 15:40:50 +0100

    Updated showTricks and template to display all tricks

--- src/Controller/WikiController.php
+++ src/Controller/WikiController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TrickRepository;
+use App\Entity\Trick;
 
 class WikiController extends AbstractController
 {
     /**
      * @Route("/", name="show_tricks")
      */
-    public function showTricks()
+    public function showTricks(TrickRepository $repo)
     {
+        $tricks = $repo->findAll();
         return $this->render('wiki/index.html.twig', [
             'controller_name' => 'WikiController',
+            'tricks' => $tricks
         ]);
     }
 }
