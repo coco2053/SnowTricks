f8df8dde3e712d4cb826fa959bd61dbae7b8bc36
Bastien Vacherand <coco2053@hotmail.com>
2018-12-21 12:56:46 +0100

    Closes #19, closes #20, closes #21; Created addEdit method, add template and Trick form

--- src/Controller/WikiController.php
+++ src/Controller/WikiController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use App\Repository\TrickRepository;
+use App\Form\TrickType;
 use App\Entity\Trick;
 
 class WikiController extends AbstractController
@@ -27,7 +30,23 @@ class WikiController extends AbstractController
     {
 
         return $this->render('wiki/show.html.twig', [
-            'trick' => $trick,
+            'trick' => $trick
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="add_trick")
+     */
+    public function addEdit(Trick $trick = null, Request $request, ObjectManager $manager)
+    {
+        if (!$trick) {
+            $trick = new Trick();
+        }
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        return $this->render('wiki/add.html.twig', [
+            'formTrick' => $form->createView()
         ]);
     }
 }
