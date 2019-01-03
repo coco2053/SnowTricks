7a2e8a44b23fb6c189297389e9388c1e525ad09a
Bastien Vacherand <coco2053@hotmail.com>
2019-01-03 19:08:04 +0100

    Closes #30, closes #32; Created User and AvatarImage entities.

---
+++ src/Entity/User.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ * fields= {"email"},
+ * message= "L'email que vous avez indiqué est déjà utilisé !"
+ * )
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="Votre mot de passe doit contenir au moins 8 caracteres !")
+     */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Les mots de passe entrés ne sont pas les mêmes !")
+     */
+    public $confirm_password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+
+    private $username;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActicve;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $registeredAt;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\AvatarImage", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $avatarImage;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getIsActicve(): ?bool
+    {
+        return $this->isActicve;
+    }
+
+    public function setIsActicve(bool $isActicve): self
+    {
+        $this->isActicve = $isActicve;
+
+        return $this;
+    }
+
+    public function getRegisteredAt(): ?\DateTimeInterface
+    {
+        return $this->registeredAt;
+    }
+
+    public function setRegisteredAt(\DateTimeInterface $registeredAt): self
+    {
+        $this->registeredAt = $registeredAt;
+
+        return $this;
+    }
+
+    public function getAvatarImage(): ?AvatarImage
+    {
+        return $this->avatarImage;
+    }
+
+    public function setAvatarImage(AvatarImage $avatarImage): self
+    {
+        $this->avatarImage = $avatarImage;
+
+        return $this;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+}
